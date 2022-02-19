2d99c74823ed7b0d5ab75752681380cb76e8e66c
JonatanFragoso <82913012+JonatanFragoso@users.noreply.github.com>
2022-02-19 18:48:19 -0500

    Modifiquei a urls.viwels.py para ler a pasta cadastros. funcionando.

--- appUsuario/views.py
+++ appUsuario/views.py
@@ -9,25 +9,25 @@ from django.urls import reverse_lazy
 class CadastroUsuario(CreateView):
     model = Usuario
     fields = ['nome','email','nivel','senha']
-    template_name = 'appUsuario/index_usuarios.html'
+    template_name = 'cadastros/index_usuarios.html'
     success_url = reverse_lazy('listagem')
 
 # pagina listagem - R
 class ListagemUsuario(ListView):
     model = Usuario
-    template_name = 'appUsuario/listar_usuarios.html'
+    template_name = 'cadastros/listar_usuarios.html'
 
 # pagina editar - U
 class UpdateUsuario(UpdateView):
     model = Usuario
     fields = "__all__"
-    template_name = 'appUsuario/index_usuarios.html'
+    template_name = 'cadastros/index_usuarios.html'
     success_url = reverse_lazy('listagem')
 
 # pagina deletar - D
 class DeleteUsuario(DeleteView):
     model = Usuario
-    template_name = 'appUsuario/excluir_usuarios.html'
+    template_name = 'cadastros/excluir_usuarios.html'
     success_url = reverse_lazy('listagem')
 
 def abertura_modelform(request):
