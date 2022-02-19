39b10d2dd3182ddb2bde443824d73c515dbcec07
JonatanFragoso <82913012+JonatanFragoso@users.noreply.github.com>
2022-02-19 18:17:33 -0500

    só reitirei um espaço.

--- appUsuario/models.py
+++ appUsuario/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Usuario(models.Model):
-    nome = models.CharField('Digite seu nome', max_length=200 )
-    email = models.EmailField('Digite seu email', max_length=200 )
+    nome = models.CharField('Digite seu nome', max_length=200)
+    email = models.EmailField('Digite seu email', max_length=200)
     #aprender a usar models.IntegerChoices() aqui embaixo
     nivel = models.IntegerField('Digite o nível de privilégio');
     senha = models.CharField('Digite a senha', max_length=200)
