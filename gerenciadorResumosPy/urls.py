49f521ccf3f6c7b5e4bcac1a5a1481c0479996c8
Murilovc <mvc.dev7cf@gmail.com>
2022-03-14 14:37:52 -0500

    revertendo para algo semelhante ao inicio

--- gerenciadorResumosPy/urls.py
+++ gerenciadorResumosPy/urls.py
@@ -1,4 +1,4 @@
-"""projetoweb URL Configuration
+"""gerenciadorResumosPy URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include ('pages.urls', namespace='pages')),
+    path('', 'templates/index.html')
 ]
--- gerenciadorResumosPy/wsgi.py
+++ gerenciadorResumosPy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projetoweb project.
+WSGI config for gerenciadorResumosPy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetoweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerenciadorResumosPy.settings')
 
 application = get_wsgi_application()
